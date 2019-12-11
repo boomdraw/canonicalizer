d7eba97fd3f9b53cec3902e886d2406da3641a18
Ivan Voskresensky <admin@shantal.su>
2019-12-11 00:26:32 +0000

    Add url and slug canonicalization methods

--- tests/Unit/CanonicalizerTest.php
+++ tests/Unit/CanonicalizerTest.php
@@ -4,9 +4,21 @@ namespace Boomdraw\Canonicalizer\Tests\Unit;
 
 use Boomdraw\Canonicalizer\Tests\TestCase;
 use Canonicalizer;
+use Illuminate\Support\Str;
 
 class CanonicalizerTest extends TestCase
 {
+    public function testMacro(): void
+    {
+        Canonicalizer::macro('replaceSpaces', function (string $string) {
+            return str_replace(' ', '', $string);
+        });
+        $this->assertSame(
+            Canonicalizer::replaceSpaces('Hello World!'),
+            'HelloWorld!'
+        );
+    }
+
     public function testCanonicalization(): void
     {
         $this->assertSame(
@@ -33,14 +45,36 @@ class CanonicalizerTest extends TestCase
         );
     }
 
-    public function testMacro(): void
+    public function testSlugCanonicalization(): void
     {
-        Canonicalizer::macro('replaceSpaces', function (string $string) {
-            return str_replace(' ', '', $string);
-        });
+        $this->assertSame(Canonicalizer::canonicalizeSlug('hello world'), Str::slug('hello world'));
+        $this->assertSame(Canonicalizer::canonicalizeSlug('hello-world'), Str::slug('hello-world'));
+        $this->assertSame(Canonicalizer::canonicalizeSlug('hello_world'), Str::slug('hello_world'));
+        $this->assertSame(Canonicalizer::canonicalizeSlug('hello_world', '_'), Str::slug('hello_world', '_'));
+        $this->assertSame(Canonicalizer::canonicalizeSlug('user@host'), Str::slug('user@host'));
+    }
+
+    public function testUriCanonicalization(): void
+    {
         $this->assertSame(
-            Canonicalizer::replaceSpaces('Hello World!'),
-            'HelloWorld!'
+            'hello-my-baby/hello-my-darling/lalala',
+            Canonicalizer::canonicalizeUrl('/Hello my baby!/hello_my_darling/lal&^#$ala/')
+        );
+        $this->assertSame(
+            'hello_my_baby/hello_my_darling/lalala',
+            Canonicalizer::canonicalizeUrl('/Hello my baby!/hello_my_darling/lal&^#$ala/', '_')
+        );
+    }
+
+    public function testUrlCanonicalization(): void
+    {
+        $this->assertSame(
+            Canonicalizer::canonicalizeUrl('/Hello my baby!/hello_my_darling/lal&^#$ala/'),
+            Canonicalizer::canonicalizeUri('/Hello my baby!/hello_my_darling/lal&^#$ala/')
+        );
+        $this->assertSame(
+            Canonicalizer::canonicalizeUrl('/Hello my baby!/hello_my_darling/lal&^#$ala/', '_'),
+            Canonicalizer::canonicalizeUri('/Hello my baby!/hello_my_darling/lal&^#$ala/', '_')
         );
     }
 }
